3f3a9a0c4bd943a903e1937fbd2346e3e1acb8c5
Emilyhima <149278892+Emilyhima@users.noreply.github.com>
2024-12-07 17:24:34 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_uUk2nHJFx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '52b9m11cgbkvbdoi8llcgnusnr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2bqi37mrh75p41r9jaajph3j87', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
